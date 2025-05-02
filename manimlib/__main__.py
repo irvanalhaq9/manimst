16e3582bc4812deecbcaf00d0ab6b5c0b0349372
Irvanal Haq <irvanalhaq9@gmail.com>
2025-05-02 20:06:48 +0700

    make scene_config can be modified when reloading

--- manimlib/__main__.py
+++ manimlib/__main__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from addict import Dict
 
 from manimlib import __version__
 from manimlib.config import manim_config
@@ -21,9 +20,7 @@ def run_scenes():
     """
     Runs the scenes in a loop and detects when a scene reload is requested.
     """
-    # Create a new dict to be able to upate without
-    # altering global configuration
-    scene_config = Dict(manim_config.scene)
+    scene_config = manim_config.scene
     run_config = manim_config.run
 
     if run_config.show_in_window:
